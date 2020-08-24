a510fa3ec9897e02d13d0271fdaf5c8b60621b14
daniel-j <daniel.j.hede@gmail.com>
2020-08-24 13:06:12 +0200

    use nuget package

--- script/build.bat
+++ script/build.bat
@@ -41,5 +41,5 @@ nimble test
 echo Building examples
 nimble examples
 
-copy "%src_dir%\webview\script\microsoft.web.webview2.0.9.488\build\native\x64\WebView2Loader.dll" "%src_dir%\examples\demo"
-copy "%src_dir%\webview\script\microsoft.web.webview2.0.9.488\build\native\x64\WebView2Loader.dll" "%src_dir%\examples\simple"
+copy "%src_dir%\webview\script\nuget\Microsoft.Web.WebView2.0.9.488\build\native\x64\WebView2Loader.dll" "%src_dir%\examples\demo"
+copy "%src_dir%\webview\script\nuget\Microsoft.Web.WebView2.0.9.488\build\native\x64\WebView2Loader.dll" "%src_dir%\examples\simple"
